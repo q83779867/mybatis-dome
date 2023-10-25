b4e5ea8d4b8afcde911a9670cdbea4b4e7aa8dd2
dijia <83779867@qq.com>
2023-10-25 11:43:43 +0800

    crud

--- src/main/java/com/leiou/mapper/BrandMapper.java
+++ src/main/java/com/leiou/mapper/BrandMapper.java
@@ -19,20 +19,29 @@ public interface BrandMapper {
      */
     Brand selectById(int id);
 
-    List<Brand> selectByCondition(@Param("status") int status,@Param("companyName") String companyName,@Param("brandName") String brandName);
+    //List<Brand> selectByCondition(@Param("status") int status,@Param("companyName") String companyName,@Param("brandName") String brandName);
 
     //List<Brand> selectByCondition(int status,String companyName,String brandName);
 
     //List<Brand> selectByCondition(Brand brand);
-    //List<Brand> selectByCondition(Map map);
+    List<Brand> selectByCondition(Map map);
+    //单条件动态查询
+
 
     String selectZhengjia(Brand brand);
 
     String selectShanchu(int id);
     String selectGai(Brand brand);
 
-
-
-
-
+//单条件查询
+ List<Brand> selectByConditionSingle(Brand brand);
+ //添加
+ void add(Brand brand);
+ void add1(Brand brand);
+ //修改
+ int update(Brand brand);
+ //根据id删除
+ void deleteById(int id);
+ //批量删除
+ void deleteByIds( int[] ids);
 }
--- src/test/java/com/leiou/test/MybatisTset.java
+++ src/test/java/com/leiou/test/MybatisTset.java
@@ -12,10 +12,15 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MybatisTset {
 
+
+    public MybatisTset() throws IOException {
+    }
 
     @Test
     public void testSelectAll() throws IOException {
@@ -65,13 +70,17 @@ public class MybatisTset {
         companyName = "%"+ companyName+"%";
         brandName = "%" + brandName + "%";
 
+        Map map = new HashMap();
+        //map.put("status" , status);
+        map.put("companyName", companyName);
+      //  map.put("brandName" , brandName);
 
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
         BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
-        List<Brand> brands = mapper.selectByCondition(status,companyName,brandName);
+        List<Brand> brands = mapper.selectByCondition(map);
         System.out.println(brands);
         System.out.println("模糊查询成功");
 
@@ -91,7 +100,7 @@ public class MybatisTset {
         BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
         //todo 执行方法
 
-        String aodi = mapper.selectZhengjia(new Brand(4, "奥迪", "奥迪集团", 5, "奥迪牛逼", 1));
+        String aodi = mapper.selectZhengjia(new Brand(5, "奥迪", "奥迪集团", 5, "奥迪牛逼", 1));
         System.out.println(aodi);
         System.out.println("插入成功");
 
@@ -110,7 +119,7 @@ public void  testselectShanchu() throws IOException {
     //todo 获取Mapper接口的代理对象
     BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
     //todo 执行方法
-    mapper.selectShanchu(5);
+    mapper.selectShanchu(43);
     System.out.println("删除成功");
 
     //todo 释放
@@ -137,4 +146,170 @@ public void  testselectShanchu() throws IOException {
     }
 
 
+    @Test
+    public void testSelectByConditionSingle() throws IOException {
+        //todo 接收参数
+        int status = 1;
+        String companyName = "小米";
+        String brandName = "小米";
+        companyName = "%"+ companyName+"%";
+        brandName = "%" + brandName + "%";
+
+        Brand brand = new Brand();
+      //  brand.setStatus(status);
+        //brand.setBrandName(brandName);
+        //brand.setCompanyName(companyName);
+
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        List<Brand> brands = mapper.selectByConditionSingle(brand);
+        System.out.println(brands);
+        System.out.println("模糊查询成功");
+
+        sqlSession.close();
+
+    }
+
+    @Test
+    public void add() throws IOException {
+        //todo 接收参数
+        int status = 1;
+        String companyName = "波导手机";
+        String brandName = "波导";
+        String description = "手机中的战斗机";
+        int ordered = 100;
+
+
+        Brand brand = new Brand();
+        brand.setStatus(status);
+        brand.setBrandName(brandName);
+        brand.setCompanyName(companyName);
+        brand.setDescription(description);
+        brand.setOrdered(ordered);
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        mapper.add(brand);
+        int id = brand.getId();
+        System.out.println(brand.getId());
+        System.out.println("添加成功");
+        System.out.println(mapper);
+        System.out.println("添加成功");
+
+       // sqlSession.commit();
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void add1() throws IOException {
+        //todo 接收参数
+        int status = 1;
+        String companyName = "波导手机";
+        String brandName = "波导";
+        String description = "手机中的战斗机";
+        int ordered = 100;
+
+
+        Brand brand = new Brand();
+        brand.setStatus(status);
+        brand.setBrandName(brandName);
+        brand.setCompanyName(companyName);
+        brand.setDescription(description);
+        brand.setOrdered(ordered);
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+
+        mapper.add(brand);
+        int id = brand.getId();
+        System.out.println(brand.getId());
+        System.out.println("添加成功");
+        System.out.println(mapper);
+        System.out.println("添加成功");
+
+        sqlSession.close();
+
+    }
+
+    @Test
+    public void testUpdate() throws IOException {
+        //todo 接收参数
+        int status = 1;
+        String companyName = "波导手机";
+        String brandName = "波导";
+        String description = "手机中的大垃圾";
+        int ordered = 200;
+        int id = 50;
+
+        Brand brand = new Brand();
+       brand.setStatus(status);
+        //brand.setBrandName(brandName);
+        //brand.setCompanyName(companyName);
+        brand.setDescription(description);
+        //brand.setOrdered(ordered);
+        brand.setId(id);
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+
+        int updates = mapper.update(brand);
+        System.out.println(updates);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void testDeleteById() throws IOException {
+        //todo 接收参数
+
+        int id = 46;
+
+
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+
+    mapper.deleteById(id);
+       // System.out.println(updates);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void testDeleteByIds() throws IOException {
+        //todo 接收参数
+
+        int[] ids = {47,48,49};
+
+
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+
+        mapper.deleteByIds(ids);
+        // System.out.println(updates);
+        sqlSession.commit();
+        sqlSession.close();
+    }
 }
+
